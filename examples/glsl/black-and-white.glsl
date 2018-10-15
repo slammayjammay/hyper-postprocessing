4a256bce1ea3b105321072835e89c9641ba8301e
Scott McKenzie <slammayjammay@gmail.com>
2018-10-14 19:42:14 -0700

    Fix black-and-white fragment

--- examples/glsl/black-and-white.glsl
+++ examples/glsl/black-and-white.glsl
@@ -1,5 +1,12 @@
-// turn all colors that aren't black in to white
+// turn all colors that aren't black to white
+#define threshold 0.001
 
 void mainImage(const in vec4 inputColor, const in vec2 uv, out vec4 outputColor) {
-	outputColor *= 1000.0;
+	float hasRed = step(threshold, inputColor.r);
+	float hasBlue = step(threshold, inputColor.g);
+	float hasGreen = step(threshold, inputColor.b);
+
+	float isNotBlack = clamp(hasRed + hasBlue + hasGreen, 0.0, 1.0);
+
+	outputColor = mix(inputColor, vec4(1.0), isNotBlack);
 }
